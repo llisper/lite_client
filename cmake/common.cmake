a2b0277d06c7bb3136dc2a6d44c1c440cce6dd81
Llisper Zhang <llisperzhang@gmail.com>
2014-03-12 22:08:56 +0800

    reorganize cmake scripts

---
+++ cmake/common.cmake
@@ -0,0 +1,32 @@
+macro(use_dlog m_name source_list)
+  set(module_name ${m_name})
+  include_directories(${CMAKE_CURRENT_BINARY_DIR})
+  list(APPEND ${source_list} 
+    ${CMAKE_CURRENT_BINARY_DIR}/use_dlog.h
+    ${CMAKE_CURRENT_BINARY_DIR}/use_dlog.cc
+    )
+
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/public/config/use_dlog.h.conf
+    ${CMAKE_CURRENT_BINARY_DIR}/use_dlog.h
+    NEWLINE_STYLE UNIX
+    )
+
+  configure_file(
+    ${CMAKE_SOURCE_DIR}/public/config/use_dlog.cc.conf
+    ${CMAKE_CURRENT_BINARY_DIR}/use_dlog.cc
+    NEWLINE_STYLE UNIX
+    )
+endmacro(use_dlog m_name)
+
+macro(r_add_dirs)
+  file(GLOB cmakelists
+    RELATIVE ${CMAKE_CURRENT_LIST_DIR}
+    */CMakeLists.txt
+    )
+
+  foreach(ml ${cmakelists})
+    get_filename_component(dir ${ml} PATH)
+    add_subdirectory(${dir})
+  endforeach(ml ${cmakelists})
+endmacro(r_add_dirs)
